75c0b6665c1dc62753b230f43b3f2989013eb99a
Rod Vagg <rod@vagg.org>
2019-12-30 15:10:27 +1100

    update deps & add linting

--- update/add-route.js
+++ update/add-route.js
@@ -1,30 +1,28 @@
 const hyperquest = require('hyperquest')
-    , bl         = require('bl')
-    , qs         = require('querystring')
+const bl = require('bl')
+const qs = require('querystring')
 
-    , url        = `https://api.mailgun.net/v3/routes`
-
+const url = 'https://api.mailgun.net/v3/routes'
 
 function addRoute (domain, creds, description, expression, actions, callback) {
-  var params  = {
-          description
-        , expression
-        , action : actions
-      }
-    , data    = qs.stringify(params)
-    , options = {
-          auth    : `api:${creds["api-key"]}`
-        , headers : {
-              'content-type': 'application/x-www-form-urlencoded'
-            , 'content-length': Buffer.byteLength(data)
-          }
-      }
+  const params = {
+    description,
+    expression,
+    action: actions
+  }
+  const data = qs.stringify(params)
+  const options = {
+    auth: `api:${creds['api-key']}`,
+    headers: {
+      'content-type': 'application/x-www-form-urlencoded',
+      'content-length': Buffer.byteLength(data)
+    }
+  }
 
-  var req = hyperquest.post(url, options)
+  const req = hyperquest.post(url, options)
 
   req.pipe(bl(callback))
   req.end(data)
 }
-
 
 module.exports = addRoute
