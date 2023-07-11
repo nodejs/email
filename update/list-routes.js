107b480696e3884b0352a4ccf2d11b8f2d955947
Ulises Gascon <ulisesgascongonzalez@gmail.com>
2023-07-11 08:12:38 +0200

    chore: replace credentials.json for a environmental variable

--- update/list-routes.js
+++ update/list-routes.js
@@ -2,8 +2,8 @@ const jsonist = require('jsonist')
 
 const url = 'https://api.mailgun.net/v3/routes'
 
-function listRoutes (domain, creds, callback) {
-  const options = { auth: `api:${creds['api-key']}` }
+function listRoutes (domain, callback) {
+  const options = { auth: `api:${cprocess.env.MAILGUN_API_KEY}` }
   jsonist.get(url, options, callback)
 }
 
