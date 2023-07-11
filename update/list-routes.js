36a1c5c1c6ddaff0f9b1d86c260352a829f470a7
Ulises Gascon <ulisesgascongonzalez@gmail.com>
2023-07-11 08:20:02 +0200

    fix: typo

--- update/list-routes.js
+++ update/list-routes.js
@@ -3,7 +3,7 @@ const jsonist = require('jsonist')
 const url = 'https://api.mailgun.net/v3/routes'
 
 function listRoutes (domain, callback) {
-  const options = { auth: `api:${cprocess.env.MAILGUN_API_KEY}` }
+  const options = { auth: `api:${process.env.MAILGUN_API_KEY}` }
   jsonist.get(url, options, callback)
 }
 
